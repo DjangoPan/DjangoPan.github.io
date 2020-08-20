b4ab5c288a1717c854ec75bc5903141b4eb9549a
DjangoPan <djangopan@gmail.com>
2020-08-20 16:42:14 +0800

    Site updated: 2020-08-20 16:42:14

---
+++ js/src/dytitle.js
@@ -0,0 +1,22 @@
+/*
+* @Author: Django
+* @Date:   2019-08-12 19:27:59
+* @Last Modified by:   Django
+* @Last Modified time: 2019-08-12 19:28:07
+*/
+var OriginTitile = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        $('[rel="shortcut icon"]').attr('href', "/TEP.png");
+        document.title = 'w(ﾟДﾟ)w 出BUG啦！！！！';
+        clearTimeout(titleTime);
+    }
+    else {
+        $('[rel="shortcut icon"]').attr('href', "/favicon.png");
+        document.title = '♪(^∇^*)又好了. . . ' + OriginTitile;
+        titleTime = setTimeout(function () {
+            document.title = OriginTitile;
+        }, 2000);
+    }
+});
