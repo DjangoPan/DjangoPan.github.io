51d6a1118dabe913165339bdbcf2ebde09304205
DjangoPan <djangopan@gmail.com>
2020-08-26 14:52:16 +0800

    Site updated: 2020-08-26 14:52:15

--- js/src/utils.js
+++ js/src/utils.js
@@ -126,7 +126,7 @@ NexT.utils = NexT.$u = {
       'www.youtube.com',
       'player.vimeo.com',
       'player.youku.com',
-      'music.163.com',
+      //'music.163.com',
       'www.tudou.com'
     ];
     var pattern = new RegExp( SUPPORTED_PLAYERS.join('|') );
